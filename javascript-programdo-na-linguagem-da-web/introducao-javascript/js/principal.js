070fadaa49935d136780b0d7609d22f545c96558
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-12-18 18:08:21 -0300

    181220201808

--- javascript-programdo-na-linguagem-da-web/introducao-javascript/js/principal.js
+++ javascript-programdo-na-linguagem-da-web/introducao-javascript/js/principal.js
@@ -1,34 +1,43 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++){
+    
+    var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var pesoValido = true;
-var alturaValida = true;
+    var tdImc = paciente.querySelector(".info-imc");
 
-if(peso  <= 0 || peso >= 1000){
-    console.log("peso inválido");
-    pesoValido = false;
-    tdImc.textContent = "Peso Inválido";
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if(peso  <= 0 || peso >= 1000){
+        console.log("peso inválido");
+        pesoValido = false;
+        tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura <= 0 || altura >= 3.0){
+        console.log("altura inválida");
+        alturaValida = false;
+        tdImc.textContent = "Altura Inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoValido && alturaValida){    
+        var imc = peso/(altura * altura);
+        tdImc.textContent = imc.toFixed(2);        
+    }
 }
 
-if(altura <= 0 || altura >= 3.0){
-    console.log("altura inválida");
-    alturaValida = false;
-    tdImc.textContent = "Altura Inválida";
-}
 
-if(pesoValido && alturaValida){    
-    var imc = peso/(altura * altura);
-    tdImc.textContent = imc;
-}
 
-console.log(imc);
+
