1cdc4a1ae9c3f6bd79079dad925ebcee0bf2b4aa
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-12-21 12:40:57 -0300

    211220201240

--- javascript-programdo-na-linguagem-da-web/introducao-javascript/js/form.js
+++ javascript-programdo-na-linguagem-da-web/introducao-javascript/js/form.js
@@ -7,11 +7,14 @@ botaoAdicionar.addEventListener("click", function(envet){
    
     var pacienteTr = montaTr(paciente);
 
-    var erros = validaPaciente(paciente);//continua aqui
+    var erros = validaPaciente(paciente);
 
-    if(erro.length > 0){
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erro;
+    console.log(erros);
+
+    if(erros.length > 0){
+       
+        exibeMensagensDeErro(erros);
+
         return;
     }
 
@@ -21,8 +24,22 @@ botaoAdicionar.addEventListener("click", function(envet){
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 
 });
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
 function obtemPacienteDoFormulario(form){
     
@@ -63,12 +80,28 @@ function validaPaciente(paciente){
     
     var erros = [];
 
+    if(paciente.nome.length == 0){
+        erros.push("O nome não pode ser em branco");
+    }
+
     if(!validaPeso(paciente.peso)){
         erros.push("Peso é inválido");
     }
 
     if(!validaAltura(paciente.altura)){
         erros.push("Altura é inválida");
+    }
+
+    if(paciente.gordura.length == 0){
+        erros.push("A gordura não pode ser em branco.");
+    }
+
+    if(paciente.peso.length == 0){
+        erros.push("O peso não pode ser em branco.");
+    }
+
+    if(paciente.altura.length == 0){
+        erros.push("A altura não pode ser em branco.");
     }
 
     return erros;
